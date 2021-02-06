34d6274cacdd879d574a9e26ef0deb160aed6dc1
franciscoaraujo <chicoaraujo1063@gmail.com>
2021-02-06 16:21:16 -0300

    primeiro commit

---
+++ src/test/java/br/com/e2dp/SpringBootMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.e2dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
