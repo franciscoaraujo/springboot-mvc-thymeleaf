5fa430d3d16a28f56c49cbd70f119554daecbedd
franciscoaraujo <chicoaraujo1063@gmail.com>
2021-02-06 19:15:37 -0300

    Adicinando os Services

---
+++ src/main/java/br/com/e2dp/service/FuncionarioService.java
@@ -0,0 +1,18 @@
+package br.com.e2dp.service;
+
+import java.util.List;
+
+import br.com.e2dp.domain.Funcionario;
+
+public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+}
